79e1fbfedf77614c02d30005b116e1076a597af2
ThatipallyPravalika <pravalika092001@gmail.com>
2024-06-21 17:30:46 +0530

    my files

---
+++ pravalika.py
@@ -0,0 +1,15 @@
+class pravalika:
+    rno=18
+    name="pravalika"
+    branch="cs"
+print("rno=",pravalika.rno)
+print("name=",pravalika.name)
+print("Branch=",pravalika.branch)
+print("reading....")
+
+        
+
+
+
+
+
